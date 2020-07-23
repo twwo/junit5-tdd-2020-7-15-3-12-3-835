07502f61a33eb9dc2c4fc730edc2b3d5831bfb39
LISH5 <727904507@qq.com>
2020-07-23 13:23:48 +0800

    [FizzBuzzWhizz][ShaoLi] feature:implement should_return_whizz_when_play_fizzbuzzwhizz_given_7.

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -9,6 +9,9 @@ public class FizzBuzzWhizz {
         if (number % 5 == 0) {
             return "Buzz";
         }
+        if (number % 7 == 0) {
+            return "Whizz";
+        }
         return String.valueOf(number);
     }
 }
